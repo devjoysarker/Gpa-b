bd83eea588dd6197fcdde530903a894336586595
devjoysarker <joysarker2927@gmail.com>
2022-03-30 14:17:02 +0600

    u basic javascript

---
+++ app.js
@@ -0,0 +1,91 @@
+
+// console.log(confirm('vai apni ki raje'));
+
+// console.log(prompt('select your name-1'));
+// console.log(prompt('select your name-2'));
+// console.log(prompt('select your name-3'));
+// console.log(prompt('select your name-4'));
+
+// var name =prompt ('name');
+// var food = prompt ('food');
+
+
+// console.log('my neme is '+ name +' i love ' + food );
+
+// let name = prompt('name');
+// let age = prompt('age');
+// let skill = prompt('skill');
+
+
+// console.log(`My name is ${name} & i am ${age} old my skill ${skill} `);
+
+// let roll= prompt('Plz submit your roll');
+
+
+// console.log(`Joy chandra sarker ${roll} you pass`);
+
+// let alu = 200;
+// let lao = 300;
+// let bagun = 350;
+
+// let mot = alu+lao+bagun;
+
+// console.log(`
+
+// aluo r dam =${alu}
+// lao  r dam =${lao}
+// bagun r dam =${bagun}
+// .......................
+// Total bill hoiche = ${mot}
+
+// `);
+
+// let age=13;
+
+// if(age >=18){
+// console.log(
+//     'apnir janno toh aito ayojon'
+// );
+// }else{
+//     console.log('tumni bachha ghor fera jio');
+// }
+
+// let age=prompt ('age plz');
+// if(age >= 18){
+//     console.log(`apnir biyer boysh hoiche nischinta biya kron`);
+// }else{
+//  console.log(`sabdhan apnir boys hoynai daya kora ${18 -age} bosor opekkha kron`);
+// }
+
+
+
+
+
+let name= prompt('Type your name');
+let roll=prompt('Type your Roll');
+let _gpa =prompt('Type your gpa');
+
+console.log(`Hi ${name} your roll ${roll} apni payachen`);
+
+ if(_gpa >=0 && _gpa <=33){
+     console.log(`F`);
+ }else if(_gpa >=33 && _gpa <=40){
+     console.log(`D`);
+ }else if(_gpa >=40 && _gpa < 50){
+     console.log(`C`);
+ }else if(_gpa >=50 && _gpa <60){
+     console.log(`A-`);
+ }else if(_gpa >=60 && _gpa < 70){
+     console.log(`A`);
+ }
+ else if(_gpa >=70 && _gpa < 80){
+     console.log(`A+`);
+ }
+ else if(_gpa >=80 && _gpa <90){
+     console.log(`A+`);
+ }
+ else if(_gpa==100){
+     console.log(`U r Great divison`);
+ }else{
+     console.log(`Result not found`);
+ }
---
+++ result.js
@@ -0,0 +1,24 @@
+// let marks = prompt ('put your marks');
+
+// if(marks >=0 && marks <=33) {
+//     console.log(`apni fail korechen .... rishka chalan`);
+// }else if(marks >=33 && marks <=40) {
+//     console.log(`apni d payachen`);
+// }else if(marks >=40 && marks <50){
+//     console.log('apni C payachen');
+// }else if(marks >=50 && marks <60){
+//     console.log(`apni B payachen`);
+// }else if(marks >=60 && marks <70){
+//     console.log(`apni A- payachen`);
+// }else if(marks >=70 && marks <80){
+//     console.log(`apni A payachen`);
+// }
+// else if(marks >=80 && marks <90){
+//     console.log(`apni A+ payachen`);
+// }
+// else if(marks >=90 && marks <100){
+//     console.log(`apni A+ payachen`);
+// }
+// else{
+//     console.log(`Vaii apni aita ki paichen...apni ki porikkha disilen`);
+// }
